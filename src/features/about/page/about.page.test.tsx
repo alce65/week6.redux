9d664f9ecdab0b2865f877f01bd1f3ed7dc0acf9
Alejandro Cerezo <alce65@hotmail.es>
2022-11-08 17:40:23 +0100

    Update test with Redux Context

--- src/features/about/page/about.page.test.tsx
+++ src/features/about/page/about.page.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter as Router } from 'react-router-dom';
 import Home from './about.page';
+import { Provider } from 'react-redux';
+import { appStore } from '../../../infrastructure/store/store';
 
-describe('Given Home component', () => {
+describe('Given About component', () => {
     describe('When we render the component', () => {
         beforeEach(() => {
             render(
-                <Router>
-                    <Home />
-                </Router>
+                <Provider store={appStore}>
+                    <Router>
+                        <Home />
+                    </Router>
+                </Provider>
             );
         });
         test('Then it should display the title', () => {
