5b15d52b1af08d6ceffc8dc3091a4e27bcfcd31b
Alejandro Cerezo <alce65@hotmail.es>
2022-11-08 13:41:37 +0100

    Add lazy routes

--- src/infrastructure/components/routes/app.routes.tsx
+++ src/infrastructure/components/routes/app.routes.tsx
@@ -1,13 +1,37 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import HomePage from '../../../features/home/page/home.page';
-import AboutPage from '../../../features/about/page/about.page';
+import React from 'react';
 
+const Home = React.lazy(() => import('../../../features/home/page/home.page'));
+const About = React.lazy(
+    () => import('../../../features/about/page/about.page')
+);
 export function AppRoutes() {
     return (
         <Routes>
-            <Route path="home" element={<HomePage></HomePage>}></Route>
-            <Route path="about" element={<AboutPage></AboutPage>}></Route>
-            <Route path="" element={<HomePage></HomePage>}></Route>
+            <Route
+                path="home"
+                element={
+                    <React.Suspense>
+                        <Home></Home>
+                    </React.Suspense>
+                }
+            ></Route>
+            <Route
+                path="about"
+                element={
+                    <React.Suspense>
+                        <About></About>
+                    </React.Suspense>
+                }
+            ></Route>
+            <Route
+                path=""
+                element={
+                    <React.Suspense>
+                        <Home></Home>
+                    </React.Suspense>
+                }
+            ></Route>
             <Route path="*" element={<Navigate replace to="" />}></Route>
         </Routes>
     );
